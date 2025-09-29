a32bea7ab6b848490787f48a32eb787ed6f96f6d
Игорь Ёшин <whybear95@gmail.com>
2025-09-29 13:54:25 +0300

    Update lab1.java

--- lab1.java
+++ lab1.java
@@ -1,4 +1,21 @@
-public class lab1 {
+public class lab13 {
+
+    public static float fills2(int arg1, float arg2){
+        switch(arg1){
+                    case 9 : return (float) Math.pow((4/Math.cbrt(arg2/2)),3); 
+                    case 3, 4, 7, 8, 10, 14, 15 : return (float) Math.pow(((1/2) * Math.sin(Math.asin((arg2 + 0.5)/27))),Math.cbrt(Math.log(Math.abs(arg2)))); 
+                    default : return (float) (Math.cos(Math.pow((Math.E),(Math.pow((3/arg2),2))))) * (float) (Math.pow(((Math.pow(((1/4) * (Math.tan(arg2) - 1)),3))/(Math.pow(arg2,3) - (2/3))),3) - 1);
+                }
+
+    }
+
+    public static void printmessage(float[][] mass){
+        for(int t = 0; t < 14; t++){
+            for(int r = 0; r < 20; r++){
+                System.out.printf("%.5f%n", mass[t][r]);
+        }
+    }
+}
     public static void main(String[]args){
         
         int[] s1 = new int[14];
@@ -18,18 +35,12 @@ public class lab1 {
         float[][] s2 = new float[14][20];
         for(int i = 0; i < 14; i++){
             for(int j = 0; j < 20; j++){
-                switch(s1[i]){
-                    case 9: s2[i][j] = (float) Math.pow((4/Math.cbrt(x[j]/2)),3);
-                    case 3, 4, 7, 8, 10, 14, 15: s2[i][j] = (float) Math.pow(((1/2) * Math.sin(Math.asin((x[j] + 0.5)/27))),Math.cbrt(Math.log(Math.abs(x[j]))));
-                    default: s2[i][j] = (float) (Math.cos(Math.pow((Math.E),(Math.pow((3/x[j]),2))))) * (float) (Math.pow(((Math.pow(((1/4) * (Math.tan(x[j]) - 1)),3))/(Math.pow(x[j],3) - (2/3))),3) - 1);
-                }
+                s2[i][j] = fills2(s1[i], x[j]);
+
             }
         }
         
-        for(int t = 0; t < 14; t++){
-            for(int r = 0; r < 20; r++){
-                System.out.printf("%.5f%n", s2[t][r]);
-            }
-        }
+        printmessage(s2);
+}
 }
 }
